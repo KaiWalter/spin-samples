47aad5e1c6a5d117c832d4f55872e45e2047483c
Kai Walter <kai.walter@outlook.de>
2023-12-02 19:32:31 +0000

    forwarding messages

--- ts-dapr/src/index.ts
+++ ts-dapr/src/index.ts
@@ -1,33 +1,66 @@
-import { HandleRequest, HttpRequest, HttpResponse, Router } from "@fermyon/spin-sdk"
+import {
+  HandleRequest,
+  HttpRequest,
+  HttpResponse,
+  Router,
+  Config,
+} from "@fermyon/spin-sdk";
 
-const encoder = new TextEncoder()
-const decoder = new TextDecoder()
-const router = Router()
+const encoder = new TextEncoder();
+const decoder = new TextDecoder();
+const router = Router();
 
-router.get("/healthz", () => ({ status: 200 }))
-router.options("/q-order-ingress", () => ({ status: 200 }))
-router.post("/q-order-ingress", ({ }, body) => formatJson(body))
+router.get("/healthz", () => ({ status: 200 }));
+router.options("/q-order-ingress", () => ({ status: 200 }));
+router.post("/q-order-ingress", ({}, body) => messageHandler(body));
 router.all("*", () => ({
-    status: 404,
-    body: encoder.encode("Not found")
-}))
+  status: 404,
+  body: encoder.encode("Not found"),
+}));
 
-function formatJson(body: ArrayBuffer): HttpResponse {
-    try {
-        const json = JSON.parse(decoder.decode(body))
-        console.log(json); 
-        return {
-            status: 200,
-            body: encoder.encode(JSON.stringify(json, null, 2))
-        }
-    } catch (e) {
-        return {
-            status: 400,
-            body: encoder.encode("Invalid JSON provided, sorry!")
-        }
-    }
+async function messageHandler(body: ArrayBuffer): Promise<HttpResponse> {
+  try {
+    const order = JSON.parse(decoder.decode(body));
+    console.log(order);
+
+    const dapr_url = Config.get("dapr_url");
+    console.log(dapr_url);
+    const url = `${dapr_url}/v1.0/bindings/q-order-${order.delivery}`;
+    console.log(url);
+
+    let result = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: encoder.encode(
+        JSON.stringify(
+          {
+            operation: "create",
+            data: order,
+          },
+          null,
+          2,
+        ),
+      ),
+    });
+
+    console.log("after fetch");
+
+    return {
+      status: 200,
+      body: encoder.encode(JSON.stringify(order, null, 2)),
+    };
+  } catch (e) {
+    return {
+      status: 500,
+      body: encoder.encode("internal error"),
+    };
+  }
 }
 
-export const handleRequest: HandleRequest = async function (request: HttpRequest): Promise<HttpResponse> {
-    return router.handleRequest(request, request.body)
-}
+export const handleRequest: HandleRequest = async function (
+  request: HttpRequest,
+): Promise<HttpResponse> {
+  return router.handleRequest(request, request.body);
+};
--- ts-dapr/testbinding.sh
+++ ts-dapr/testbinding.sh
@@ -4,7 +4,19 @@ curl -X POST http://localhost:3501/v1.0/bindings/q-order-ingress \
   -H "Content-Type: application/json" \
   -d '{
         "data": {
-          "name": "Hi"
+          "delivery": "standard"
+        },
+        "metadata": {
+          "ttlInSeconds": "60"
+        },
+        "operation": "create"
+      }'
+
+curl -X POST http://localhost:3501/v1.0/bindings/q-order-ingress \
+  -H "Content-Type: application/json" \
+  -d '{
+        "data": {
+          "delivery": "express"
         },
         "metadata": {
           "ttlInSeconds": "60"
