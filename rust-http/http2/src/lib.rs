8d9c6a87a75e2340728b703ee4c96cf03926c22e
Kai Walter <kai.walter@outlook.de>
2023-12-02 15:50:11 +0000

    prepare not succeed rust dapr call

--- rust-http/http2/src/lib.rs
+++ rust-http/http2/src/lib.rs
@@ -1,6 +1,9 @@
 use anyhow::Result;
 use spin_sdk::{
-    http::{IntoResponse, Json, Params, Request, Response, Router},
+    http::{
+        IncomingResponse, IntoResponse, Json, Method, Params, Request, RequestBuilder, Response,
+        Router,
+    },
     http_component, variables,
 };
 
@@ -22,10 +25,22 @@ fn health(_req: Request, _params: Params) -> Result<impl IntoResponse> {
 }
 
 fn ingress(req: http::Request<Json<Order>>, _params: Params) -> Result<impl IntoResponse> {
-    let dapr_port_var = variables::get("dapr_url")?;
+    let dapr_url = variables::get("dapr_url")?;
 
     println!("name: {}", req.body().name);
-    println!("port_var: {}", dapr_port_var);
+    println!("dapr_url: {}", dapr_url);
+
+    // let body = bytes::Bytes::from(json!({ "name": "test" }).to_string());
+    // let url = format!("{}/v1.0/bindings/q-order-standard", dapr_url);
+    // let res = spin_sdk::http::send(Request::post(url, Some(body)));
+    //
+    // let request = RequestBuilder::new(Method::Post, "/v1.0/bindings/q-order-standard")
+    //     .uri(dapr_url)
+    //     .method(Method::Post)
+    //     .body("Hello, world!")
+    //     .build();
+    //
+    // let _response: IncomingResponse = spin_sdk::http::send(request).await?;
 
     Ok(Response::new(200, format!("name: {}", req.body().name)))
 }
