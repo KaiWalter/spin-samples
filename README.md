2f973709a38def7fca27dd47ad4678e693df24f0
Kai Walter <kai.walter@outlook.de>
2023-12-03 15:43:23 +0000

    build to ACR

--- README.md
+++ README.md
@@ -8,8 +8,26 @@ rustup target add wasm32-wasi
 wget -q https://raw.githubusercontent.com/dapr/cli/master/install/install.sh -O - | /bin/bash
 dapr init
 
+## install Docker and make docker build wasm ready
+
+from <https://github.com/deislabs/containerd-wasm-shims/issues/87#issuecomment-1510404441>
+
+```
+curl -fsSL https://test.docker.com -o test-docker.sh
+sudo sh test-docker.sh
+```
+
+```
+sudo sh -c "cat >>/etc/docker/daemon.json" <<-EOF
+{
+    "features": {
+        "containerd-snapshotter": true
+    }
+}
+EOF
+```
+
 ## Links
 
 <https://dev.to/thangchung/how-to-run-webassemblywasi-application-spin-with-dapr-on-kubernetes-2b8n>
 <https://github.com/fermyon/spin>
-
---
+++ ts-dapr/build-docker.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -e
+
+source <(cat $(git rev-parse --show-toplevel)/.env)
+
+RESOURCE_GROUP_NAME=`az group list  --query "[?starts_with(name,'$AZURE_ENV_NAME')].name" -o tsv`
+AZURE_CONTAINER_REGISTRY_NAME=`az resource list --tag azd-env-name=$AZURE_ENV_NAME --query "[?type=='Microsoft.ContainerRegistry/registries'].name" -o tsv`
+AZURE_CONTAINER_REGISTRY_ENDPOINT=`az acr show -n $AZURE_CONTAINER_REGISTRY_NAME --query loginServer -o tsv`
+
+az acr login -n $AZURE_CONTAINER_REGISTRY_NAME
+
+if [[ -z $(docker buildx ls | grep wasm-builder) ]]; then
+  docker buildx create --name wasm-builder --platform wasi/wasm
+fi
+
+docker buildx use wasm-builder
+docker buildx build --platform=wasi/wasm --push -t $AZURE_CONTAINER_REGISTRY_ENDPOINT/ts-dapr .
+docker buildx use default
