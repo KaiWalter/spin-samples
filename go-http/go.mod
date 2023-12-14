90f02e0fe7af73de6c41ba1ada87c657f8ff911a
Kai Walter <kai.walter@outlook.de>
2023-12-14 19:22:51 +0000

    deploy all 3 services

---
+++ go-http/go.mod
@@ -0,0 +1,7 @@
+module github.com/go_http
+
+go 1.20
+
+require github.com/fermyon/spin/sdk/go/v2 v2.0.1
+
+require github.com/julienschmidt/httprouter v1.3.0 // indirect
--- ts-dapr/deploy-k3d-from-acr.sh
+++ ts-dapr/deploy-k3d-from-acr.sh
@@ -11,8 +11,13 @@ AZURE_CONTAINER_REGISTRY_ENDPOINT=`az acr show -n $AZURE_CONTAINER_REGISTRY_NAME
 TAG=`az acr repository show-tags -n $AZURE_CONTAINER_REGISTRY_NAME --repository $APP --top 1 --orderby time_desc -o tsv`
 IMAGE_NAME=$AZURE_CONTAINER_REGISTRY_ENDPOINT/$APP:$TAG
 
+cat workload-k3d.yml | \
 yq eval ".spec|=select(.selector.matchLabels.app==\"distributor\")
-                 .template.spec.containers[0].image =
-                    \"$IMAGE_NAME\"" workload-k3d.yml | kubectl apply -f -
+    .template.spec.containers[0].image = \"$IMAGE_NAME\"" | \
+yq eval ".spec|=select(.selector.matchLabels.app==\"receiver-express\") 
+    .template.spec.containers[0].image = \"$IMAGE_NAME\"" | \
+yq eval ".spec|=select(.selector.matchLabels.app==\"receiver-standard\")
+    .template.spec.containers[0].image = \"$IMAGE_NAME\"" | \
+kubectl apply -f -
 
 kubectl describe pod -l=app=distributor
